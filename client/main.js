2ab0d1a56c472454047629aba529e542218ff0cd
chaewon0128 <hjh7814@naver.com>
2023-01-13 15:04:44 +0900

    주접생성기

--- client/main.js
+++ client/main.js
@@ -1,3 +1,26 @@
-console.log(window);
+import { getNode, getInputValue, getRandom,insertLast } from "./lib/index.js";
+import { jujeobData } from "./data/data.js";
 
 
+const submit = getNode('#submit')
+const result = getNode('.result')
+
+
+
+function submitHandler(e) {
+  e.preventDefault();
+let name = getInputValue('#nameField')
+let list = jujeobData(name)
+
+
+let pick = list[getRandom(list.length-1)]; //!이거 다시 보쟈
+
+console.log(pick);
+
+result.textContent = ''
+insertLast(result,pick)
+
+}
+
+
+submit.addEventListener('click',submitHandler)
