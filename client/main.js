8d6b46c102a07adbe397fc4c9d5d990cd6dcad70
chaewon0128 <hjh7814@naver.com>
2023-01-13 15:34:20 +0900

    이름 입력 validation 추가

--- client/main.js
+++ client/main.js
@@ -1,26 +1,30 @@
-import { getNode, getInputValue, getRandom,insertLast } from "./lib/index.js";
+import { getNode, getInputValue, getRandom,insertLast, clearContents, syntaxError, isNumericString } from "./lib/index.js";
 import { jujeobData } from "./data/data.js";
+
 
 
 const submit = getNode('#submit')
 const result = getNode('.result')
 
-
-
 function submitHandler(e) {
   e.preventDefault();
 let name = getInputValue('#nameField')
 let list = jujeobData(name)
-
-
 let pick = list[getRandom(list.length-1)]; //!이거 다시 보쟈
 
-console.log(pick);
+if(!name) {
+  alert('이름을 입력해주세요')
+  return // 오류니까 리턴 써서 종료
+}
+if(isNumericString(name)) {
+  alert('제대로된 이름을 입력 하세요');
+  return // 오류니까 리턴 써서 종료
+}
 
-result.textContent = ''
+
+clearContents(result)
 insertLast(result,pick)
 
 }
-
 
 submit.addEventListener('click',submitHandler)
